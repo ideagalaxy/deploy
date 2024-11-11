b4e6e6826a6a1f0b16ee27bea3100f87e0f87384
Kim Minsung <ideagalaxy@naver.com>
2024-11-11 23:56:18 +0900

    real ready

--- cccapp/settings.py
+++ cccapp/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import os
 
