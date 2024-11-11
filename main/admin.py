131ebf0df5c09dff3b2bb9580c355ab5c9b5a512
ideagalaxy <51442651+ideagalaxy@users.noreply.github.com>
2024-11-11 23:16:42 +0900

    Delete main directory

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-from django.contrib.auth.admin import UserAdmin
-
-# Register your models here.
-admin.site.register(User, UserAdmin)
-admin.site.register(Person)
-admin.site.register(BankBook)
-admin.site.register(Exchange)
